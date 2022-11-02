a5ca16f1c7c697eb7ce635a1a880a1ee4a48297f
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-02 15:33:48 +0530

    Implemented create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: :index, param: :slug
+  resources :tasks, only: [:index, :create], param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
