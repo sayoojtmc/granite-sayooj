30b6506e85a3b240d693f8e0eb6b4c54d7a74c4a
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-02 17:18:03 +0530

    Added show page for a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,11 @@ class TasksController < ApplicationController
     render status: :ok, json: { tasks: tasks }
   end
 
+  def show
+    task = Task.find_by!(slug: params[:slug])
+    respond_with_json({ task: task })
+  end
+
   def create
     task = Task.new(task_params)
     task.save!
--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const list = () => axios.get("/tasks");
+const show = slug => axios.get(`/tasks/${slug}`);
 const create = payload =>
   axios.post("/tasks", {
     task: payload,
   });
 
-const tasksApi = { list, create };
+const tasksApi = { list, show, create };
 
 export default tasksApi;
