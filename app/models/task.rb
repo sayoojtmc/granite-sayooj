f6ade11d2ed4c781f0256ff03411bfade92c840c
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-04 11:59:49 +0530

    Added association in User model for assigned tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :slug, uniqueness: true
+  belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   before_create :set_slug
   validate :slug_not_changed
 
