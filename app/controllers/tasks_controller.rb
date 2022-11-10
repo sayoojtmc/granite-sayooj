39a3e754501277c829a002d5951de54eee97fe39
Sayooj Surendran <sayoojtmc@gmail.com>
2022-11-10 13:14:07 +0530

    Configured Sidekiq for background job processing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,7 @@ class TasksController < ApplicationController
   after_action :verify_policy_scoped, only: :index
   before_action :load_task!, only: %i[show update destroy]
   before_action :ensure_authorized_update_to_restricted_attrs, only: :update
+
   def index
     tasks = policy_scope(Task)
     @pending_tasks = tasks.includes(:assigned_user).of_status(:pending)
